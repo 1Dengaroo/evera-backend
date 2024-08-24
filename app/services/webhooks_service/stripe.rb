6ee15c1cf555b8f07011b113028cd2f0da464735
Andy Deng <andydengaroo@gmail.com>
2024-08-23 23:05:48 -0400

    Create address and delivery on order completion

--- app/services/webhooks_service/stripe.rb
+++ app/services/webhooks_service/stripe.rb
@@ -14,6 +14,23 @@ module WebhooksService
         product = item.product
         product.update(quantity: product.quantity - item.quantity)
       end
+
+      shipping_details = session['shipping_details']
+      address = Address.create!(
+        city: shipping_details['address']['city'],
+        country: shipping_details['address']['country'],
+        line1: shipping_details['address']['line1'],
+        line2: shipping_details['address']['line2'],
+        postal_code: shipping_details['address']['postal_code'],
+        state: shipping_details['address']['state'],
+        name: shipping_details['name']
+      )
+
+      order.create_delivery!(
+        email: session['customer_details']['email'],
+        address:,
+        session_id: session.id
+      )
     end
   end
 end
