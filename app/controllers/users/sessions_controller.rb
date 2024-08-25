beaa4a778758ee7f8b3cd47993c1d0261e2df4df
1Dengaroo <denga@bc.edu>
2024-08-25 15:46:11 -0400

    Add users

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  include RackSessionsFix
+  respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    render json: {
+      status: {
+        code: 200, message: 'Logged in successfully',
+        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
+      }
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    return render json: { status: 401, message: 'No token provided' }, status: :unauthorized if request.headers['Authorization'].blank?
+
+    token = request.headers['Authorization'].split(' ').last
+    return render json: { status: 401, message: 'Malformed token' }, status: :unauthorized unless token.present? && token.split('.').size == 3
+
+    begin
+      jwt_payload = JWT.decode(token, Rails.application.credentials.devise_jwt_secret_key!, true, { algorithm: 'HS256' }).first
+      current_user = User.find(jwt_payload['sub'])
+    rescue JWT::DecodeError
+      return render json: { status: 401, message: 'Invalid token' }, status: :unauthorized
+    end
+
+    if current_user
+      render json: { status: 200, message: 'Logged out successfully' }, status: :ok
+    else
+      render json: { status: 401, message: 'Session expired' }, status: :unauthorized
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_24_022722) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_25_155848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -25,6 +25,18 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_24_022722) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "deliveries", force: :cascade do |t|
@@ -71,6 +83,21 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_24_022722) do
     t.text "sub_images", default: [], array: true
     t.string "sizes", default: ["XS", "S", "M", "L", "XL", "XXL"], array: true
     t.integer "quantity", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "jti", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "deliveries", "addresses"
