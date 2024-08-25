ad47a4fb2cd9af54eda88dc311fbc23e240ef527
Andy Deng <andydengaroo@gmail.com>
2024-08-25 16:18:37 -0400

    Create db seed

--- db/schema.rb
+++ db/schema.rb
@@ -27,18 +27,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_25_155848) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "deliveries", force: :cascade do |t|
     t.string "email", null: false
     t.string "status", default: "manufacturing", null: false
